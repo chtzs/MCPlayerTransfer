a8dbefe1060606684cfa57df9e96ff8f6e8f0dbf
chtzs <wsbcht@sina.com>
2023-01-26 13:36:11 +0800

    Add dependency

--- src/main/java/top/hackchen/Main.java
+++ src/main/java/top/hackchen/Main.java
@@ -1,7 +1,21 @@
 package top.hackchen;
 
+import net.querz.nbt.tag.ByteTag;
+import net.querz.nbt.tag.CompoundTag;
+import net.querz.nbt.tag.DoubleTag;
+import net.querz.nbt.tag.Tag;
+
+import java.util.function.BiConsumer;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        CompoundTag ct = new CompoundTag();
+
+        ct.put("byte", new ByteTag((byte) 1));
+        ct.put("double", new DoubleTag(1.234));
+        ct.putString("string", "stringValue");
+        ct.forEach((s, tag) -> System.out.println(s));
+        System.out.println(ct);
     }
 }
